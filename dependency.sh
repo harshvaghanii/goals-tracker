e6355bab95c3d55bee3e63773a242994b61d9b60
harshvaghanii <harshvaghani98@gmail.com>
2025-01-12 11:53:15 -0500

    initial commit

---
+++ dependency.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Move to Repository root directory
+
+cd /Users/harshvaghani/Desktop/programmingprojects/devops/dummyapps/goal-tracker
+
+# Move to backend directory
+
+cd backend
+
+# Install Backend directory
+
+npm install
+
+# Move to Frontend directory
+
+cd ../frontend
+
+# Install Frontend Directory
+
+npm install
